79479b83cbdbe74312e64a010569e79ef1a3f725
UHanczar <u.hanczar@gmail.com>
2017-08-08 01:00:16 +0300

    fixing_citiesList

--- src/components/RenderCities.js
+++ src/components/RenderCities.js
@@ -3,7 +3,7 @@ import React from 'react';
 import City from './City';
 
 const RenderCities = (props) => {
-  const citiesList = props.cities.reverse();
+  const citiesList = props.cities.slice().reverse();
   return (
     <div>
       {citiesList.map(city => <City key={city} city={city} removeCity={props.removeCity} />)}
