b89a568541db14f551b386e1f999e38de60f4315
UHanczar <u.hanczar@gmail.com>
2017-08-08 22:54:15 +0300

    fixing_for_heroku

--- server.js
+++ server.js
@@ -8,17 +8,17 @@ const PORT = process.env.PORT || 3000;
 // calling express library
 const app = express();
 
-// using express middleware for redirectiong openweathermap traffic from https to http
-app.use((req, res, next) => {
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect(`http://${req.hostname}${req.url}`);
-  } else {
-    next();
-  }
-});
-
-// telling, which folder we wanna run
-app.use(express.static('public'));
+// // using express middleware for redirectiong openweathermap traffic from https to http
+// app.use((req, res, next) => {
+//   if (req.headers['x-forwarded-proto'] === 'https') {
+//     res.redirect(`http://${req.hostname}${req.url}`);
+//   } else {
+//     next();
+//   }
+// });
+// 
+// // telling, which folder we wanna run
+// app.use(express.static('public'));
 
 // starting the server using port and callback function
 app.listen(PORT, () => {
